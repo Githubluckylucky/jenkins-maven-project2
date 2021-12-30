1123eca16a96c3b28957a57683c958f08458d829
Githubluckylucky <sunita.ray.bbsr@gmail.com>
2021-12-30 18:01:20 +0530

    first commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,6 @@
+public class Application {
+
+    public static void main(String a[]) {
+        System.out.println("Helloworld");
+    }
+}
---
+++ src/test/java/TestApplication.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class TestApplication {
+    @Test
+    public void test1() {
+        System.out.println("Helloworld test");
+    }
+}
